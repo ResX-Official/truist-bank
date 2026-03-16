9edcbfb1aff8ae00488bda09d640f9dcee132e18
Ebylex <chigozieebube9@gmail.com>
2026-03-16 22:18:26 +0100

    fix: polyfill __dirname for Edge Runtime in next-intl plugin

--- bank/next.config.ts
+++ bank/next.config.ts
@@ -4,7 +4,16 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const nextConfig: NextConfig = {
-  // no additional config needed
+  webpack: (config, { nextRuntime, webpack }) => {
+    if (nextRuntime === "edge") {
+      config.plugins.push(
+        new webpack.DefinePlugin({
+          __dirname: JSON.stringify("/"),
+        })
+      );
+    }
+    return config;
+  },
 };
 
 export default withNextIntl(nextConfig);
